dd75b2c9411226f64af98da5df9bb3161e992467
will <jwshepherd172002@gmail.com>
2025-12-08 13:01:57 -0600

    .

--- src/types/page.ts
+++ src/types/page.ts
@@ -1,16 +1,3 @@
-/**
- * const usersPage: Page<User> = {
- *   data: [
- *     { id: 1, name: 'Alice' },
- *     { id: 2, name: 'Bob' }
- *   ],
- *   page: 1,
- *   pageSize: 25,
- *   pageCount: 4,
- *   totalCount: 100
- * };
- */
-
 export interface Page<T> {
     data: T[];
     page: number;
